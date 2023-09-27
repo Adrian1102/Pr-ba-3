ad270b2804427dfaa99fd1330de04e752d30aba8
unknown <300adrianmach@gmail.com>
2023-09-27 09:48:07 +0200

    Added kodilla-testing.calculator and updated test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
-public class TestingMain {
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //new test for Calculator
+
+        Calculator calculator = new Calculator();
+
+
+        int additionResult = calculator.add(5, 3);
+        if (additionResult == 8) {
+            System.out.println("Add function works");
+        } else {
+            System.out.println("Add function doesnt work");
+        }
+
+        int subtractionResult = calculator.subtract(10, 4);
+        if (subtractionResult == 6) {
+            System.out.println("Subtract function works");
+        } else {
+            System.out.println("Subtract function doesnt work.");
+        }
     }
 }
